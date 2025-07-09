7e68212f9242e4c45ca10a109f76025eae16fe25
AblabiX <83076368+AblabiX@users.noreply.github.com>
2025-07-09 21:46:34 +0000

    Add: Italian language (#28)
    * Create it.json
    
    Italian translation
    
    * Update i18next.js
    
    added it.json
    
    * Update it.json
    
    added new strings

--- app/i18next/i18next.js
+++ app/i18next/i18next.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from './en.json';
+import it from './it.json';
 import fr from './fr.json';
 
 i18n.use(initReactI18next).init({
@@ -9,6 +10,7 @@ i18n.use(initReactI18next).init({
   fallbackLng: 'en',
   resources: {
     en: en,
+    it: it,
     fr: fr,
   },
   interpolation: {

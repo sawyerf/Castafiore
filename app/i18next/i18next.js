43e3bc089721285d68ea5f95f14b9e73d38bbe5f
chryuvski <shenyaodi@live.com>
2025-07-11 18:52:22 +0800

    Add: Chinese localization (#40)
    * Add: Chinese localization
    
    both Chinese(Simplified) and Chinese(Traditional)
    
    * add: zh setting choice

--- app/i18next/i18next.js
+++ app/i18next/i18next.js
@@ -5,6 +5,8 @@ import en from './en.json';
 import it from './it.json';
 import fr from './fr.json';
 import ru from './ru.json';
+import zhHans from './zh-Hans.json';
+import zhHant from './zh-Hant.json';
 
 i18n.use(initReactI18next).init({
   lng: 'en',
@@ -14,6 +16,8 @@ i18n.use(initReactI18next).init({
     it: it,
     fr: fr,
     ru: ru,
+    zhHans: zhHans,
+    zhHant: zhHant,
   },
   interpolation: {
     escapeValue: false

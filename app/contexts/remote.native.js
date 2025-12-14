70334529c34cb820931827676466cf23dd9ef992
Sawyerf <sawyer.flink@protonmail.ch>
2025-12-14 23:32:51 +0100

    Fix: handle transfer error

--- app/contexts/remote.native.js
+++ app/contexts/remote.native.js
@@ -1,12 +1,27 @@
 import React from 'react'
+
 import { SongContext, SongDispatchContext } from '~/contexts/song'
 import { ConfigContext } from '~/contexts/config'
-
+import logger from '~/utils/logger'
 import Player from '~/utils/player'
 
 const RemoteContext = React.createContext()
 
 let isTransferring = false
+
+const transfer = async (fromDevice, toDevice, config, song, songDispatch) => {
+	isTransferring = true
+	// Action on previous player
+	const savedState = await Player.saveState()
+	await Player.stopSong()
+	await Player.disconnect(fromDevice)
+
+	// Action on current player
+	songDispatch({ type: 'setPlaying', state: Player.State.Loading })
+	await Player.connect(toDevice, toDevice?.type || 'local')
+	await Player.playSong(config, songDispatch, song.queue, song.index)
+	await Player.restoreState(savedState)
+}
 
 export const RemoteProvider = ({ children }) => {
 	const [selectedDevice, setSelectedDevice] = React.useState(null)
@@ -24,20 +39,16 @@ export const RemoteProvider = ({ children }) => {
 
 		const hasSong = song?.queue && song?.index !== undefined && song?.songInfo
 		if (hasSong || config?.url) {
-			(async () => {
-				isTransferring = true
-				// Action on previous player
-				const savedState = await Player.saveState()
-				await Player.stopSong()
-				await Player.disconnect(prevDevice)
-
-				// Action on current player
-				songDispatch({ type: 'setPlaying', state: Player.State.Loading })
-				await Player.connect(currentDevice, currentDevice?.type || 'local')
-				await Player.playSong(config, songDispatch, song.queue, song.index)
-				await Player.restoreState(savedState)
-				isTransferring = false
-			})()
+			transfer(prevDevice, currentDevice, config, song, songDispatch)
+				.then(() => isTransferring = false)
+				.catch((error) => {
+					logger.error('RemoreContext', 'Error during device transfer', error)
+					isTransferring = false
+					prevSelectedDeviceRef.current = null
+					setSelectedDevice(null)
+					Player.connect(null, 'local')
+					songDispatch({ type: 'setPlaying', state: Player.State.Stopped })
+				})
 		}
 	}, [selectedDevice])
 
@@ -46,6 +57,7 @@ export const RemoteProvider = ({ children }) => {
 		selectedDevice,
 		selectDevice: (device) => {
 			if (isTransferring) return false
+			if (selectedDevice?.id === device?.id) return false
 			setSelectedDevice(device)
 			return true
 		},

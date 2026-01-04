6276d4e6a35a7dcae643e8feb0e5cd33da423e9a
Sawyerf <sawyer.flink@protonmail.ch>
2026-01-04 14:21:35 +0100

    Change order language

--- app/screens/Settings/Language.js
+++ app/screens/Settings/Language.js
@@ -19,11 +19,11 @@ const languages = [
 	{ lang: 'fr', name: 'Français', color: '#007bff', flag: '🇫🇷' },
 	{ lang: 'gl', name: 'Galego', color: '#2195ddff', flag: '🇪🇸' },
 	{ lang: 'it', name: 'Italiano', color: '#28a745', flag: '🇮🇹' },
+	{ lang: 'ko', name: '한국어', color: '#0047A0', flag: '🇰🇷' },
 	{ lang: 'ptBr', name: "Português do Brasil", color: '#302681', flag: '🇧🇷' },
 	{ lang: 'ru', name: 'Русский', color: '#ffc107', flag: '🇷🇺' },
 	{ lang: 'zhHans', name: '简体中文', color: '#FF0000', flag: '🇨🇳' },
 	{ lang: 'zhHant', name: '正體中文', color: '#0000AA', flag: '🇹🇼' },
-	{ lang: 'ko', name: '한국어', color: '#0047A0', flag: '🇰🇷' },
 ]
 
 const sysLang = localeLang()

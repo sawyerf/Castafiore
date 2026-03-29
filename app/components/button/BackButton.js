4bd5832c834d6fc6cb1d892430b2ad2e04a82839
silentrald <silentrald1216@gmail.com>
2026-03-29 15:21:46 +0800

    Fix: visible ui icons for same colored background

--- app/components/button/BackButton.js
+++ app/components/button/BackButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useTheme } from '~/contexts/theme'
 
 import IconButton from '~/components/button/IconButton'
 import size from '~/styles/size'
@@ -8,19 +9,26 @@ import size from '~/styles/size'
 const BackButton = () => {
 	const navigation = useNavigation()
 	const insets = useSafeAreaInsets()
+	const theme = useTheme()
 
 	return (
 		<IconButton
 			style={{
 				position: 'absolute',
+				width: 50,
+				height: 50,
 				top: insets.top,
 				left: insets.left,
-				padding: 20,
+				borderRadius: '50%',
+				padding: 10,
+				margin: 10,
 				zIndex: 2,
+				alignItems: 'center',
+				backgroundColor: `${theme.backgroundTouch}c0`, // add 75% opacity
 			}} onPress={() => navigation.goBack()}
 			icon="chevron-left"
 			size={size.icon.small}
-			color="#fff"
+			color={theme.primaryTouch}
 		/>
 	)
 }
